da8a141f397e794ffff8599563b8298233c7455b
christieewen <christieewen@users.noreply.github.com>
2014-02-23 11:37:32 -0500

    Update searchByProgram.py
    Prints out schools' printed name and Program Highlights

--- searchByProgram.py
+++ searchByProgram.py
@@ -2,7 +2,6 @@ __author__ = 'Christie'
 # http://www.pythonforbeginners.com/python-on-the-web/parsingjson/
 import urllib3
 #import urllib2
-from bs4 import BeautifulSoup
 import json
 
 #url = 'http://nycdoe.pediacities.com/api/action/datastore_search?limit=5&q=ELL%20Data:ELS'
@@ -10,23 +9,20 @@ import json
 #url = 'http://nycdoe.pediacities.com/api/action/datastore_search_sql?sql=SELECT%20%22Printed_Name%22,%22BUS%22,%22SUBWAY%22%20from%20%2245f6a257-c13a-431b-acb9-b1468c3ff1e9%22'
 url = 'http://nycdoe.pediacities.com/api/action/datastore_search_sql?sql=SELECT%20%22Printed_Name%22,%22Program%20Highlights%22%20from%20%2245f6a257-c13a-431b-acb9-b1468c3ff1e9%22'
 
-
 http = urllib3.PoolManager()
 
 r = http.request('GET', url)
+
+#print (type(r.data))
 
 #Python 3 requires explicit Bytes to String conversions
 # See http://www.rmi.net/~lutz/strings30.html
 j = json.loads(r.data.decode())
 
-print (j.keys())
-#print (type(j))
-print (j['result'])
-#soup = BeautifulSoup(r.data)
-#print (soup)
+#print (j.keys())
 
+#print (j['result'])
 
-#print (soup.contents)
-#print r.status
-#print r.headers['server']
-#print r.data
+for k in j['result']['records']:
+    print(k['Printed_Name'] + " " + k['Program Highlights'] + "\n")
+
